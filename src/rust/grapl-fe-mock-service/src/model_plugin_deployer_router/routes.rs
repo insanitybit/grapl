5712f2589b943777e6739c6522bb4e2458948e9e
andrea <andrea@penguin>
2021-06-17 18:33:05 -0600

    /deploy

--- src/rust/grapl-fe-mock-service/src/model_plugin_deployer_router/routes.rs
+++ src/rust/grapl-fe-mock-service/src/model_plugin_deployer_router/routes.rs
@@ -1,9 +1,5 @@
 use ::reqwest;
-use actix_web::{
-    post,
-    HttpResponse,
-    Responder,
-};
+use actix_web::{post, HttpResponse, Responder};
 use serde::{
     Deserialize,
     Serialize,
@@ -11,8 +7,8 @@ use serde::{
 
 use crate::{
     make_request,
-    request_with_body,
 };
+// use actix_web::web::Json;
 
 #[derive(Serialize, Deserialize)]
 pub struct DeployRequest {
@@ -35,47 +31,40 @@ pub enum PluginError {
 }
 
 // accept http requests, translate to grpc requests.
+#[derive(Serialize, Deserialize)]
+pub struct PluginDeployerResp {
+    plugin_name: String,
+    response: String,
+}
 
-// actix procedural macros that route incoming http requests
+#[derive(Serialize, Deserialize)]
+pub struct PostBody {
+    body: String
+}
+
+    // struct PluginSuccessfullyDeleted {
+    //     plugin_to_delete: String,
+    //     message: Boolean,
+    // }
+
+
 #[post("/modelPluginDeployer/deploy")]
-pub async fn grapl_model_plugin_deployer(
-    body: actix_web::web::Json<DeployRequest>,
-) -> impl Responder {
-    // CALL MODEL-PLUGIN-DEPlOYER GRPC CLIENT
-    let body = body.into_inner();
-    let response = request_with_body("deploy", body).await;
-
-    match response {
-        Ok(response) => HttpResponse::Ok().json(response),
-
-        Err(PluginError::InvalidSchema) => HttpResponse::BadRequest().finish(),
-
-        Err(PluginError::ReadError) => HttpResponse::Conflict().finish(),
-
-        Err(PluginError::ServerError) => HttpResponse::BadRequest().finish(),
-
-        Err(PluginError::RequestError(_)) => HttpResponse::InternalServerError().finish(),
-    }
+pub async fn grapl_model_plugin_deployer(_req: actix_web::web::Json<PostBody>) -> impl Responder {
+    HttpResponse::Ok().json(PluginDeployerResp{
+       plugin_name: String::from("test_plugin"),
+       response: String::from("success"),
+   }).await
 }
 
-#[post("/modelPluginDeployer/deletePlugin")]
-pub async fn delete_plugin(body: actix_web::web::Json<DeployRequest>) -> impl Responder {
-    let body = body.into_inner();
 
-    let response = request_with_body("delete", body).await;
+// #[post("/modelPluginDeployer/deletePlugin")]
+// pub async fn delete_plugin(body: Json<PostBody>) -> Result<web::Json<PluginSuccessfullyDeleted>, E> {
+//     Ok(web::Json(PluginSuccessfullyDeleted{
+//         plugin_to_delete: String::from("test_plugin"),
+//         message: String::from("success"),
+//     }))
+// }
 
-    match response {
-        Ok(response) => HttpResponse::Ok().json(response),
-
-        Err(PluginError::InvalidSchema) => HttpResponse::BadRequest().finish(),
-
-        Err(PluginError::ReadError) => HttpResponse::Conflict().finish(),
-
-        Err(PluginError::ServerError) => HttpResponse::BadRequest().finish(),
-
-        Err(PluginError::RequestError(_)) => HttpResponse::InternalServerError().finish(),
-    }
-}
 // actix procedural macros that route incoming http requests
 #[post("/modelPluginDeployer/listPlugins")]
 pub async fn list_plugin() -> impl Responder {
@@ -93,20 +82,3 @@ pub async fn list_plugin() -> impl Responder {
         Err(PluginError::RequestError(_)) => HttpResponse::InternalServerError().finish(),
     }
 }
-
-// We will make a post request to our new actix server
-// This will route us to the appropriate model plugin deployer service.
-
-// we come in on a path. Based on that path, we route the request to the appropriate service.
-
-// X route to the graplModelPluginDeployer
-// X set up docker stuff
-
-// setup & write tests with an http client
-// use grpc client for model-plugin-deployer
-
-// add more logging
-// documentation
-
-// NEXT: Framework for integration
-// When will Actix4 Be released? and what is it?
